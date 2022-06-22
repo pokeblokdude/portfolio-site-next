59eb2dc0644859441d5e5f2f8bda22b9ef5054b1
Alex Howe <howie242000@gmail.com>
2022-06-22 19:57:55 -0400

    Updated home page w/ bio

--- pages/index.js
+++ pages/index.js
@@ -30,97 +30,92 @@ export default function Home() {
       </div>
       <div className="w-full max-w-[1366px] flex flex-col items-center mb-4">
         
-        <h1 className="w-[95%] text-2xl font-semibold mt-6 mb-4">Recent Projects</h1>
+        <div className='w-[95%] flex justify-between mobile:flex-col mobile:items-center'>
 
-        <div className="flex flex-col w-[95%] items-center">
-          <h2 className="w-full pl-1 mb-4 font-mono">Super Mario Galaxy - As Good as We Remember?</h2>
-          <div 
-            className='w-full flex flex-col justify-center items-center h-[360px] relative'
-          >
-            <Image src='/bg/bg-smg.png' layout='fill' className='object-cover object-center pointer-events-none' alt=''/>
-            <div
-              className="w-full h-full bg-black/60 opacity-0 hover:cursor-pointer hover:opacity-100 hover:transition-all 
-                transition duration-300 py-16 px-40 narrow:px-20 backdrop-blur flex flex-col z-10"
-              onClick={() => {
-                setVideoUrl('https://youtu.be/OSq1M7QD2wM');
-                setModalOpen(true);
-              }}
-            >
-              <p className="text-sm text-neutral-400 font-mono mb-2 w-full max-w-[1366px] self-center">Aug. 2021.</p>
-              <p className="leading-relaxed max-w-[1366px] self-center">
-                <span className="font-mono font-bold italic">Super Mario Galaxy - As Good as We Remember? </span> 
-                is a long-form video essay reflecting on and analyzing my experience playing the 2007 Nintendo game,
-                <span className="italic"> Super Mario Galaxy.</span> It covers everything from level design to graphics, 
-                sound and gameplay.
+          <div className='w-[48%] h-full items-center flex flex-col mobile:w-[95%]'>
+            <h1 className='w-[95%] text-2xl font-semibold mt-6 mb-4'>Bio</h1>
+            <div className='w-[90%] flex flex-col justify-center items-center h-[500px] relative ml-2 min:w-[95%] mobile:h-[700px] mobile:w-[90%] mobile:ml-0'>
+              <Image src='/me.jpg' layout='fill' className='object-cover object-top' alt='' priority={true} loading='eager' quality={100}/>
+            </div>
+            <h2 className="w-[90%] pl-1 mt-2 font-mono text-sm text-neutral-400">Me, playing disc golf</h2>
+            <div className='flex flex-col ml-1 w-[90%]'>
+              <p className='mt-4 leading-relaxed min:leading-relaxed w-[95%]'>
+                Hi there! My name is Alex Howe. I&apos;m a filmmaker and game developer living in western Massachusetts,
+                currently attending UMass Amherst for a degree in Computer Science and Film Studies.
               </p>
-              <p className="leading-relaxed mt-2 max-w-[1366px] self-center">
-                Intended for release on YouTube, I tried to switch up the &quot;internet video essay 
-                formula&quot; by incorporating cinematics captured using&nbsp;
-                <Link href='https://noclip.website'><a target='_blank' className="font-semibold hover:underline">noclip.website,</a></Link>
-                &nbsp;in addition to more traditional gameplay b-roll.
+              <p className='mt-2 leading-relaxed min:leading-relaxed w-[95%]'>
+                In my free time, I make videos on YouTube, work on games and film ideas, and play video games.
               </p>
-              <p className="leading-loose mt-2 max-w-[1366px] w-full self-center">
-                The video&apos;s sountrack is comprised of music from the game.
-              </p>
-              <p className="text-sm text-neutral-400 font-mono mt-2 w-full max-w-[1366px] self-center">Click to view.</p>
             </div>
           </div>
-        </div>
-        <div className='flex w-[95%] mt-4'>
-          <div className="flex flex-col w-1/2 items-center">
-            <h2 className="w-full pl-1 mb-4 font-mono">UKG Job Referral Tool</h2>
-            <div 
-              className='w-full flex flex-col justify-center items-center h-[300px] relative'
-            >
-              <Image src='/bg/bg-ukg.PNG' layout='fill' className='object-cover object-top' alt=''/>
-              <div
-                className="w-full h-full bg-black/90 opacity-0 hover:opacity-100 hover:transition-all 
-                  transition duration-300 py-16 px-20 narrow:py-14 narrow:px-16 min:py-12 backdrop-blur flex flex-col z-10"
+
+          <hr className='border-dotted mt-8 w-[95%] desktop:hidden'></hr>
+
+          <div className='w-1/2 flex flex-col mobile:w-[95%]'>
+
+            <h1 className="w-[95%] text-2xl font-semibold mt-6 mb-4">Recent Projects</h1>
+
+            <div className="flex flex-col w-full items-center">
+              <h2 className="w-full pl-1 mb-4 font-mono">Cataclysm</h2>
+              <div 
+                className='w-full flex flex-col justify-center items-center h-[300px] relative'
               >
-                <p className="text-sm text-neutral-400 font-mono mb-2 w-full max-w-[1366px] self-center">Dec. 2021.</p>
-                <p className="leading-relaxed min:leading-normal w-full max-w-[600px] self-center">
-                  <span className='font-mono font-bold italic'>Job Referral Tool</span> &nbsp;was a semester-long project
-                  for CS320 - Software Engineering at UMass Amherst. Students were split into teams of 10, each tasked with
-                  creating a prototype for an internal job referral app for the HR company, UKG.
-                </p>
-                <div className='mt-4 flex w-full max-w-[600px] self-center'>
-                  <Link href='https://job-listing-tool-320.herokuapp.com'>
-                    <a target='_blank' className="text-sm text-neutral-400 font-mono hover:underline hover:cursor-pointer">View Heroku deployment.</a>
-                  </Link>
-                  <Link href='https://github.com/elinottonson/job-listing-tool'>
-                    <a target='_blank' className="text-sm text-neutral-400 font-mono ml-5 hover:underline hover:cursor-pointer">View source on GitHub.</a>
-                  </Link>
+                <Image src='/bg/bg-cataclysm.jpg' layout='fill' className='object-cover object-center' alt='' priority={true} loading='eager' quality={100}/>
+                <div
+                  className="w-full h-full bg-black/80 opacity-0 hover:opacity-100 hover:transition-all 
+                    transition duration-300 py-16 px-20 narrow:px-28 backdrop-blur z-10"
+                >
+                  <p className="text-sm text-neutral-400 font-mono mb-2 w-full max-w-[1366px] self-center">Feb. 2022.</p>
+                  <p className="leading-loose min:leading-relaxed">
+                    <span className='font-mono font-bold italic'>Cataclysm</span> &nbsp;is a small game
+                    about a cat causing havoc. It was developed in 3 days by me and four of my friends, and submitted to&nbsp;
+                    <Link href='https://itch.io/jam/mini-jame-gam-7'>
+                      <a target='_blank' className='font-semibold hover:underline hover:cursor-pointer'>Mini Jame Gam #7,</a>
+                    </Link>
+                    &nbsp;with the theme, &quot;You Are the Enemy.&quot;
+                  </p>
+                  <div className='mt-6 flex'>
+                    <Link href='https://poke-bd.itch.io/cataclysm'>
+                      <a target='_blank' className="text-sm text-neutral-400 font-mono hover:underline hover:cursor-pointer">Play on Itch.io.</a>
+                    </Link>
+                    <Link href='https://github.com/pokeblokdude/cat-game'>
+                      <a target='_blank' className="text-sm text-neutral-400 font-mono hover:underline hover:cursor-pointer ml-8">View source on GitHub.</a>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="flex flex-col w-1/2 items-center">
-            <h2 className="w-full pl-1 mb-4 font-mono">Odyssey 2D</h2>
-            <div 
-              className='w-full flex flex-col justify-center items-center h-[300px] relative'
-            >
-              <Image src='/bg/bg-odyssey2d.jpg' layout='fill' className='object-cover object-bottom' alt=''/>
-              <div
-                className="w-full h-full bg-black/90 opacity-0 hover:opacity-100 hover:transition-all
-                  transition duration-300 py-16 px-20 narrow:py-14 narrow:px-16 backdrop-blur z-10"
-              >
-                <p className="text-sm text-neutral-400 font-mono mb-2 w-full max-w-[1366px] self-center">2021.</p>
-                <p className="leading-relaxed">
-                  <span className='font-mono font-bold italic'>Odyssey 2D</span> &nbsp;is a 2D platformer with
-                  acceleration-based movement. The player can walk, crouch, jump (variable height), 
-                  wall-slide, wall-jump, ledge-grab and air-dive. All of this was implemented using a finite state machine.
-                </p>
-                <div className='mt-4 flex'>
-                  <Link href='https://github.com/pokeblokdude/2d-game'>
-                    <a target='_blank' className="text-sm text-neutral-400 font-mono hover:underline hover:cursor-pointer">View source on GitHub.</a>
-                  </Link>
+            <div className='flex w-full mt-4'>
+              <div className="flex flex-col w-full items-center">
+                <h2 className="w-full pl-1 mb-4 font-mono">Super Mario Galaxy 2 - Nintendo&apos;s Line in the Sand</h2>
+                <div 
+                  className='w-full flex flex-col justify-center items-center h-[300px] relative'
+                >
+                  <Image src='/bg/bg-smg2.png' layout='fill' className='object-cover object-center' alt='' loading='eager' priority={true} quality={100}/>
+                  <div
+                    className="w-full h-full bg-black/60 opacity-0 hover:cursor-pointer hover:opacity-100 hover:transition-all 
+                      transition duration-300 py-16 px-20 backdrop-blur z-10"
+                    onClick={() => {
+                      setVideoUrl('https://youtu.be/6hVOmstk7qs');
+                      setModalOpen(true);
+                    }}
+                  >
+                    <p className="text-sm text-neutral-400 font-mono mb-2 w-full max-w-[1366px] self-center">Jan. 2022.</p>
+                    <p className="leading-relaxed min:leading-relaxed">
+                      <span className="font-mono font-bold italic">Super Mario Galaxy 2 - Nintendo&apos;s Line in the Sand </span> 
+                      is a follow-up to my previous video on <span className="italic">Super Mario Galaxy</span>,
+                      this time covering the game&apos;s sequel, <span className="italic"> Super Mario Galaxy 2,</span> and how it
+                      compares to the 2007 original.
+                    </p>
+                    <p className="text-sm text-neutral-400 font-mono mt-4">Click to view.</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
         
-        <div className='flex flex-col w-[95%] self-center mt-6'>
+        <div className='flex flex-col w-[95%] self-center mt-10'>
           <hr className='w-full border-dotted mb-2'></hr>
           <div className='flex justify-between mobile:flex-col'>
             <p className=''>Contact</p>
