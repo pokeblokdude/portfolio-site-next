075a5a3b37a847dff553ad993601092d25a20473
Alex Howe <howie242000@gmail.com>
2022-08-15 23:24:08 -0400

    Removed odyssey2d and golf game because they are shit

--- pages/game/index.js
+++ pages/game/index.js
@@ -33,7 +33,7 @@ const Game = () => {
                 &nbsp;with the theme, &quot;You Are the Enemy.&quot;
               </p>
               <p className='leading-loose min:leading-relaxed mt-4'>
-                The game is 3D with 2D, paper-like graphics inspired by the <span className='italic'>Paper Mario</span> series. The goal is to
+                The game is 3D, with 2D, paper-like graphics inspired by the <span className='italic'>Paper Mario</span> series. The goal is to
                 jump around and knock stuff over, causing the guy to walk over and fix it. If you distract him for long enough, you can light the
                 oven on fire to burn down the house. At first we were planning on making it a puzzle game, but eventually we settled on something
                 more akin to <span className='italic'>Untitled Goose Game,</span> where the goal is basically just to cause havoc.
@@ -49,73 +49,6 @@ const Game = () => {
             </div>
           </div>
         </div>
-
-        <div className="flex flex-col w-full items-center mt-10">
-          <h2 className="w-[95%] pl-1 mb-4 font-mono">01. &quot;Odyssey 2D&quot;</h2>
-          <div 
-            className='w-[95%] flex flex-col justify-center items-center h-[540px] relative'
-          >
-            <Image src='/bg/bg-odyssey2d.jpg' layout='fill' className='object-cover object-bottom' alt='' priority={true} loading='eager' quality={100}/>
-            <div
-              className="w-full h-full bg-black/90 opacity-0 hover:opacity-100 hover:transition-all 
-                transition duration-300 py-20 px-40 narrow:px-28 backdrop-blur z-10"
-            >
-              <p className="text-sm text-neutral-400 font-mono mb-2 w-full max-w-[1366px] self-center">2021.</p>
-              <p className="leading-loose min:leading-relaxed">
-                <span className='font-mono font-bold italic'>Odyssey 2D</span> &nbsp;is a 2D platformer with
-                acceleration-based movement, made in Unity. The original idea was to try and remake the base movement mechanics 
-                from <span className='italic'> Super Mario Odyssey</span> in a 2D environment, hence the name.
-                I also wanted to capture the &quot;weighty&quot; feel of the 3D Mario games, especially 
-                <span className='italic'> Super Mario 64.</span>
-                &nbsp;The player can walk, crouch, jump (variable height), wall-slide, wall-jump, ledge-grab and air-dive.
-                All of this was implemented using a finite state machine for the player script.
-              </p>
-              <p className='leading-loose min:leading-relaxed mt-4'>
-                All of the pixel art and animations are original, created using Aseprite. I&apos;ve toyed around a 
-                lot with 2D/3D pixel art styles, similar to&nbsp;
-                <Link href='https://www.youtube.com/watch?v=ZsMHY4LDyRE'>
-                  <a target='_blank' className='font-semibold hover:underline hover:cursor-pointer'>this prototype by &apos;t3ssel8r&apos;.</a>
-                </Link>
-                &nbsp;For now I&apos;ve stuck with 2D until I learn more about graphics and shaders.
-              </p>
-              <p className='leading-loose min:leading-relaxed mt-4'>
-                It also remains to be seen whether this ends up being a level-based game like Mario or Celeste, or more of a metroidvania
-                type of thing that&apos;s more story-driven, assuming I continue working on it.
-              </p>
-              <div className='mt-8 flex'>
-                <Link href='https://github.com/pokeblokdude/2d-game'>
-                  <a target='_blank' className="text-sm text-neutral-400 font-mono hover:underline hover:cursor-pointer">View source on GitHub.</a>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex w-[95%] self-center">
-            <div className="flex flex-col w-1/2">
-              <h2 className="w-full pl-1 mb-4 font-mono mt-10">02. Golf Game</h2>
-              <div className='w-full flex justify-center items-center h-[360px] relative'>
-                <Image src='/bg/bg-golf.jpg' layout='fill' className='object-cover object-center' alt='' loading='eager' />
-                <div
-                  className="w-full h-full bg-black/50 opacity-0 hover:opacity-100 hover:transition-all 
-                    transition duration-300 py-20 px-20 backdrop-blur z-10"
-                >
-                  <p className="text-sm text-neutral-400 font-mono mb-2 w-full max-w-[1366px] self-center">2021.</p>
-                  <p className="leading-loose min:leading-relaxed">
-                    <span className='font-mono font-bold italic'>Golf Game</span> &nbsp;is a prototype 
-                    isometric mini-golf game inspired by <span className='italic'>Golf with your Friends.</span>
-                    &nbsp; The terrain is currently built out of &quot;prefab&quot; pieces I made in Blender, which get
-                    combined into a single mesh. The game uses Unity&apos;s built-in physics system.
-                  </p>
-                  <div className='mt-8 flex'>
-                    <Link href='https://github.com/pokeblokdude/golf-game'>
-                      <a target='_blank' className="text-sm text-neutral-400 font-mono hover:underline hover:cursor-pointer">View source on GitHub.</a>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
 
         <h1 className="text-2xl font-semibold w-[95%] self-center mt-10 mb-6">CS:GO Maps</h1>
 
