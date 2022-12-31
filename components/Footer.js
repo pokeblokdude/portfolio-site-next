d96899a77a598a5eedda564bfce49e8384f1d7cd
Alex Howe <howie242000@gmail.com>
2022-12-31 18:23:29 -0500

    Made footer look better

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,9 @@ import { FaGithub } from 'react-icons/fa'
 const Footer = ({ sticky }) => {
   return (
     <div 
-      className="w-full h-14 bg-neutral-900 flex-col items-center justify-center border-t border-neutral-700 mobile:h-20"
+      className="w-full h-14 bg-neutral-900 flex items-center justify-center pt-1 border-t border-neutral-700 border-solid mobile:h-20 mobile:flex-col"
     >
-      <div className='w-full h-14 bg-neutral-900 flex items-center justify-center mobile:flex-col mobile:h-16'>
+      <div className='w-full h-14 bg-neutral-900 flex items-center justify-center mobile:flex-col mobile:h-16 mobile:mt-4'>
         <p className="font-light text-neutral-300">I made this site using Next.js and Tailwind CSS.</p>
         <p className='font-light m-2 text-neutral-300 mobile:hidden'>|</p>
         <div className='flex items-center'>
@@ -16,7 +16,7 @@ const Footer = ({ sticky }) => {
           <FaGithub className='ml-2 text-neutral-200'/>
         </div>
       </div>
-      <p className="font-light text-neutral-500 text-center text-xs mt-[-4px] mb-2">
+      <p className="font-light text-neutral-400 text-center text-xs min-w-fit mr-6 mobile:mb-4">
         Last updated: 12.31.22
       </p>
     </div>
